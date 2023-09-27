91e1ba5b6049d647db86684a0077c91d6b7c61f4
Ninuskaninus <89078328+Ninuskaninus@users.noreply.github.com>
2023-09-27 20:30:50 +0200

    Added a comment module

---
+++ js/API/post/login.js
@@ -0,0 +1,38 @@
+const API_urls = {
+  base_url: "https://api.noroff.dev",
+  register_url: "/api/v1/social/auth/register",
+  login_url: "/api/v1/social/auth/login",
+  posts_url: "/api/v1/social/posts",
+};
+
+// LOGIN
+
+const userLogin = {
+  name: "nina",
+  email: "ninamd@stud.noroff.no",
+  password: "ninaamdal12345",
+};
+
+async function loginUser(url, data) {
+  try {
+    const postData = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    };
+    const response = await fetch(url, postData);
+    console.log(response);
+    const json = await response.json();
+    const accessToken = json.accessToken;
+    localStorage.setItem("accessToken", accessToken);
+    console.log(json);
+
+    return json;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+loginUser(API_urls.base_url + API_urls.login_url, user);
