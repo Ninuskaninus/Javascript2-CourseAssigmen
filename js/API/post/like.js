6840a41fd4c4f649efbe8d90e1f8f733213d46c8
Ninuskaninus <89078328+Ninuskaninus@users.noreply.github.com>
2023-09-28 15:20:32 +0200

    trying to add script to like button

---
+++ js/API/post/like.js
@@ -0,0 +1,44 @@
+import { fetchUsersData } from "/js/users/users.js";
+const users = await fetchUsersData();
+
+// Function to update the like count and UI when a like button is clicked
+async function handleLikeButtonClick(postId) {
+    const post = users.find(post => post.postId === postId);
+  
+    if (post) {
+      // Increment the like count for the specific post
+      post.likes++;
+      
+      // Update the UI to reflect the new like count
+      const likeCountElement = document.getElementById(`likebutton${postId}`);
+      console.log(likeCountElement);
+      if (likeCountElement) {
+        likeCountElement.innerText = post.likes;
+      }
+  
+      // Send a PUT request to update the post's likes on the server
+      try {
+        const response = await fetch(`https://api.noroff.dev/api/v1/social/posts/${postId}`, {
+          method: 'PUT',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ likes: post.likes }), // Send updated likes count
+        });
+  
+        if (!response.ok) {
+          // Handle error response from the server
+          console.error('Failed to update likes on the server');
+        }
+      } catch (error) {
+        // Handle network or request error
+        console.error('Error sending PUT request:', error);
+      }
+    }
+  }
+  
+  
+
+
+
+
