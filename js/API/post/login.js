9d92d9bba616099a6e28921c62808aa97fca766f
Ninuskaninus <89078328+Ninuskaninus@users.noreply.github.com>
2023-10-02 14:56:27 +0200

    Updated login form

--- js/API/post/login.js
+++ js/API/post/login.js
@@ -1,35 +1,58 @@
-const API_urls = {
-  base_url: "https://api.noroff.dev",
-  login_url: "/api/v1/social/auth/login",
-};
+export function apiPostLogin() {
 
-/* const userLogin = {
-  name: "nina",
-  email: "ninamd@stud.noroff.no",
-  password: "ninaamdal12345",
-}; */
+const base_url = "https://api.noroff.dev/api/v1/social/auth/login";
 
-async function loginUser(url, data) {
-  try {
-    const postData = {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(data),
-    };
-    const response = await fetch(url, postData);
-    console.log(response);
-    const json = await response.json();
-    const accessToken = json.accessToken;
-    localStorage.setItem("accessToken", accessToken);
-    localStorage.setItem("userName", data.name);
-    console.log(json);
 
-    return json;
-  } catch (error) {
-    console.log(error);
+  const form = document.getElementById("loginForm");
+  const emailElement = document.getElementById("loginEmail");
+  const passwordElement = document.getElementById("loginPassword");
+  const loinBtn = document.getElementById("login-btn");
+  
+  form.addEventListener("submit", async function (event) {
+    event.preventDefault(); 
+  
+    const emailValue = emailElement.value;
+    const passwordValue = passwordElement.value;
+
+    console.log("Email Value:", emailValue);
+    console.log("Password Value:", passwordValue);
+
+    const userLogin = {
+      email: emailValue,
+      password: passwordValue,
+    }
+
+
+  async function loginUser(base_url, data) {
+    try {
+      const postData = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${accessToken}`,
+        },
+        body: JSON.stringify(userLogin),
+      };
+      const response = await fetch(base_url, postData);
+      console.log(response);
+      const json = await response.json();
+      const accessToken = json.accessToken;
+      localStorage.setItem("accessToken", accessToken);
+      localStorage.setItem("userName", data.name);
+      console.log(json);
+  
+      return json;
+    } catch (error) {
+      console.log(error);
+    }
   }
+  loginUser(base_url, userLogin);
+});
 }
 
-loginUser(API_urls.base_url + API_urls.login_url, user);
+apiPostLogin();
+
+
+
+
+
